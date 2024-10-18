fdc5bb8637c990f96d8fe8125c0aa189152c71ec
Surakattula-Rohith <srohith.charan@gmail.com>
2024-10-18 11:22:10 +0530

    Added Movie Api so that admin can handle the CRUD operation on them.

--- BookMyShowBackend/server.js
+++ BookMyShowBackend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const userRoute = require("./routes/userRoute");
+const movieRoute = require("./routes/movieRoute");
 const { validateJWTToken } = require("./middleware/authorizationMiddleware");
 
 connectDB();
@@ -11,6 +12,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/bms/users", userRoute);
+app.use("/bms/movies", validateJWTToken, movieRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT}`);
--- BookMyShowFrontEnd/src/pages/Admin/index.jsx
+++ BookMyShowFrontEnd/src/pages/Admin/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Tabs } from "antd";
+import MovieList from "./MovieList";
+import TheatreTable from "./TheatreTable";
 
-const Admin = () => {
+function Admin() {
+  const tabItems = [
+    {
+      key: "movies",
+      label: "Movies",
+      children: <MovieList />,
+    },
+    {
+      key: "theatre",
+      label: "Theatres",
+      children: <TheatreTable />,
+    },
+  ];
   return (
-    <div>Admin</div>
-  )
+    <div>
+      <h1>Admin Page</h1>
+      <Tabs items={tabItems} />
+    </div>
+  );
 }
 
-export default Admin
+export default Admin;
